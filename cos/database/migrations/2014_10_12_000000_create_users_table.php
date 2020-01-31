69963c185626f4acba5e5b1b35e04881e75bc710
unknown <storm_iz_here@yahoo.com>
2020-01-31 22:10:37 +0800

    Fixed is_staff default value.

--- cos/database/migrations/2014_10_12_000000_create_users_table.php
+++ cos/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('profile_image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('is_staff')->default(false);
+            $table->string('is_staff')->default('False');
             $table->rememberToken();
             $table->timestamps();
         });
