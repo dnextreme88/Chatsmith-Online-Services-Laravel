1936a96115326d0cb58dceb325043a8b25d67684
unknown <storm_iz_here@yahoo.com>
2020-01-31 22:04:01 +0800

    Added is_staff field (string).

--- cos/database/migrations/2014_10_12_000000_create_users_table.php
+++ cos/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('username')->unique();
-            $table->string('profile_image')->nullable(); // our profile image field
+            $table->string('profile_image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('is_staff')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
