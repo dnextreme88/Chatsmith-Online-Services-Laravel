3a29d9f8bfd8431af5dd25b04825b88b8fab7e5f
unknown <storm_iz_here@yahoo.com>
2020-01-31 22:08:51 +0800

    Renamed Time records table.

--- cos/database/migrations/2020_01_30_154049_create_time_records_table.php
+++ cos/database/migrations/2020_01_30_154049_create_time_records_table.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTimeRecordTable extends Migration
+class CreateTimeRecordsTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-        // id  time_of_shift   employee_name   date_of_shift   timestamp_in    timestamp_out   employee_id
+
     public function up()
     {
-        Schema::create('time_record', function (Blueprint $table) {
+        Schema::create('time_records', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('time_of_shift');
             $table->date('date_of_shift');
@@ -33,6 +33,6 @@ class CreateTimeRecordTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('time_record');
+        Schema::dropIfExists('time_records');
     }
 }
