a38f2654b332c4f4abd3bff6b8bab38fe47a3c76
unknown <storm_iz_here@yahoo.com>
2020-01-31 22:09:55 +0800

    is_active field is a string.

--- cos/database/migrations/2020_01_30_145432_create_employees_table.php
+++ cos/database/migrations/2020_01_30_145432_create_employees_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeeTable extends Migration
+class CreateEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateEmployeeTable extends Migration
      */
     public function up()
     {
-        Schema::create('employee', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('employee_id')->nullable();
             $table->string('first_name')->nullable();
@@ -21,7 +21,7 @@ class CreateEmployeeTable extends Migration
             $table->string('last_name')->nullable();
             $table->string('role')->nullable();
             $table->date('date_tenure')->nullable();
-            $table->boolean('is_active')->nullable();
+            $table->string('is_active')->nullable()->default('True');
             $table->timestamps();
         });
     }
@@ -33,6 +33,6 @@ class CreateEmployeeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('employee');
+        Schema::dropIfExists('employees');
     }
 }
