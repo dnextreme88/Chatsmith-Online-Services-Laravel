e8d315d80efe30c1180fbfe34202d1d8c8c8c7a5
unknown <storm_iz_here@yahoo.com>
2020-02-01 22:38:49 +0800

    Added route for AdminLog.

--- cos/routes/web.php
+++ cos/routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/admin', 'AdminPanelController@index');
+
 Route::resource('employees', 'EmployeeController');
 
 Route::resource('profile', 'ProfileController');
