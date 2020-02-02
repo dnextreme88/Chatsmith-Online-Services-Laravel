b41f891771d66fa2310167725f129a08a72c5f80
unknown <storm_iz_here@yahoo.com>
2020-02-02 16:33:43 +0800

    Added styles for Admin Panel pane. Organized main.css stylesheet for improved readability.

--- cos/resources/views/layouts/admin_panel_right_nav.blade.php
+++ cos/resources/views/layouts/admin_panel_right_nav.blade.php
@@ -1,6 +1,6 @@
-<div class="card">
-    <div class="card-header">Admin Panel</div>
-    <div class="card-body">
+<div class="card bg-primary admin-panel-card">
+    <div class="card-header admin-panel-card-header">Admin Panel</div>
+    <div class="card-body admin-panel-card-body-nav">
         <ul class="list-unstyled">
             <li class="list-item"><a href="/admin/">Admin Panel Home</a></li>
             <li class="list-item"><a href="/employees/create/">Add Employee</a></li>
