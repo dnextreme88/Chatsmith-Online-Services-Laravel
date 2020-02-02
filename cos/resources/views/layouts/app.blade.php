55a177a3ff9deb5050873ddb6d6325305025c031
unknown <storm_iz_here@yahoo.com>
2020-02-02 22:42:33 +0800

    Added favicon.

--- cos/resources/views/layouts/app.blade.php
+++ cos/resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/cos_favicon.png') }}">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
