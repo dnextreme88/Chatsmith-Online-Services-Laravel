21c0e6eaf09d3343aa881623d60f72227014784b
unknown <storm_iz_here@yahoo.com>
2020-02-04 19:39:42 +0800

    The latest announcement pane is now wrapped in speech bubbles rather than in alerts.

--- cos/resources/views/profile.blade.php
+++ cos/resources/views/profile.blade.php
@@ -6,34 +6,37 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12"> <!-- Requires $latest_announcement variable - fetch latest announcement -->
-            @include('layouts.latest_announcement_pane')
-        </div>
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Timestamps</div>
+	<div class="row justify-content-center">
+		<div class="col-md-2"> <!-- Requires $latest_announcement variable - fetch latest announcement -->
+			<img src="{{ $latest_announcement->user->image }}" class="img-thumbnail img-responsive avatar-thumbnail-small" />
+		</div>
+		<div class="col-md-10 speech-bubble">
+			@include('layouts.latest_announcement_pane')
+		</div>
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">Timestamps</div>
 
-                <div class="card-body">
-                    <!-- list of timestamps of user -->
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">Welcome, {{ $user->name }}!</div>
+				<div class="card-body">
+					<!-- list of timestamps of user -->
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-header">Welcome, {{ $user->name }}!</div>
 
-                <div class="card-body">
-                    <ul>
-                       <!-- Check if user is a staff, then show admin panel link -->
-                        @if ($user->is_staff == 'True')
-                            <li><a href="/admin/">Admin Panel</a></li>
-                        @endif
-                        <li><a href="/profile/{{ $user->id}}/edit">Settings</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+				<div class="card-body">
+					<ul>
+					   <!-- Check if user is a staff, then show admin panel link -->
+						@if ($user->is_staff == 'True')
+							<li><a href="/admin/">Admin Panel</a></li>
+						@endif
+						<li><a href="/profile/{{ $user->id}}/edit">Settings</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
