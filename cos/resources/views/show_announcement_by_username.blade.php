8506e1cb6b4a0cbd35803a297cd5a5fbe5b72986
unknown <storm_iz_here@yahoo.com>
2020-02-05 00:05:11 +0800

    Added a Read More link when Announcement description reaches more than 255 characters.

--- cos/resources/views/show_announcement_by_username.blade.php
+++ cos/resources/views/show_announcement_by_username.blade.php
@@ -14,7 +14,12 @@ Show Announcements of {{ $user->username }}
 			<div class="col-md-12 alert alert-info alert-block">
 				<h1>Title: <a href="/announcements/{{ $announcement->id }}/">{{ $announcement->title }}</a></h1>
 				<p class="text-left">Posted by <strong>{{ $announcement->user->username }}</strong> on <strong>{{ \Carbon\Carbon::parse($announcement->created_at)->format('F j, Y g:i:s A') }}</strong></p>
-				<p class="announcement-pane-description">{{ \Illuminate\Support\Str::limit($announcement->description, 255, $end='...') }}</p>
+				<p class="announcement-pane-description">
+					{{ \Illuminate\Support\Str::limit($announcement->description, 255, '.....') }}
+					@if (strlen($announcement->description) > 255)
+						<a href="/announcements/{{ $announcement->id }}/">Read More...</a>
+					@endif
+				</p>
 			</div>
 		@endforeach
 		{{ $announcements->links() }}
