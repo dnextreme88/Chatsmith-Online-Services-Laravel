e432f793996c4847538a751021c07465beff2306
unknown <storm_iz_here@yahoo.com>
2020-02-04 23:31:31 +0800

    Added AM/PM modifier on Announcement timestamps.

--- cos/resources/views/show_announcement_by_username.blade.php
+++ cos/resources/views/show_announcement_by_username.blade.php
@@ -13,7 +13,7 @@ Show Announcements of {{ $user->username }}
 		@foreach ($announcements as $announcement)
 			<div class="col-md-12 alert alert-info alert-block">
 				<h1>Title: <a href="/announcements/{{ $announcement->id }}/">{{ $announcement->title }}</a></h1>
-				<p class="text-left">Posted by <strong>{{ $announcement->user->username }}</strong> on <strong>{{ \Carbon\Carbon::parse($announcement->created_at)->format('F j, Y g:i:s') }}</strong></p>
+				<p class="text-left">Posted by <strong>{{ $announcement->user->username }}</strong> on <strong>{{ \Carbon\Carbon::parse($announcement->created_at)->format('F j, Y g:i:s A') }}</strong></p>
 				<p class="announcement-pane-description">{{ \Illuminate\Support\Str::limit($announcement->description, 255, $end='...') }}</p>
 			</div>
 		@endforeach
