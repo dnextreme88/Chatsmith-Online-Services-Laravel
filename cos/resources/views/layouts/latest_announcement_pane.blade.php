6ac3ca1997c76a237890aac8a2aadb128967dc1e
unknown <storm_iz_here@yahoo.com>
2020-02-04 23:22:14 +0800

    Modified styles for announcement pane.

--- cos/resources/views/layouts/latest_announcement_pane.blade.php
+++ cos/resources/views/layouts/latest_announcement_pane.blade.php
@@ -1,4 +1,4 @@
 <h1>Announcements!</h1>
-<p class="text-left">Posted by <strong>{{ $latest_announcement->user->username }}</strong> on <strong>{{ \Carbon\Carbon::parse($latest_announcement->created_at)->format('F j, Y g:i:s') }}</strong></p>
+<p class="text-left">Posted by <a id="announcement-pane-link" href="announcements/user/{{ $latest_announcement->user->username }}">{{ $latest_announcement->user->username }}</a> on <strong>{{ \Carbon\Carbon::parse($latest_announcement->created_at)->format('F j, Y g:i:s') }}</strong></p>
 <h3>{{ $latest_announcement->title }}</h3>
-<p class="announcement-pane-description">{{ $latest_announcement->description }}</p>
+<p id="announcement-pane-description">{{ $latest_announcement->description }}</p>
