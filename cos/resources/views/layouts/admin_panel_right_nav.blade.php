86ef32531798d018c9e5075131baea658bfcb74c
unknown <storm_iz_here@yahoo.com>
2020-02-04 18:51:42 +0800

    Nav links in the Admin Panel now have a background to it when their respective view is accessed on the webpage.

--- cos/resources/views/layouts/admin_panel_right_nav.blade.php
+++ cos/resources/views/layouts/admin_panel_right_nav.blade.php
@@ -2,12 +2,14 @@
     <div class="card-header admin-panel-card-header">Admin Panel</div>
     <div class="card-body admin-panel-card-body-nav">
         <ul class="list-unstyled">
-            <li class="list-item"><a href="/admin/">Admin Panel Home</a></li>
-            <li class="list-item"><a href="/announcements/create/">Add Announcement</a></li>
-            <li class="list-item"><a href="/employees/create/">Add Employee</a></li>
-            <li class="list-item"><a href="/announcements/">Announcements</a></li>
-            <li class="list-item"><a href="/employees/">Employees</a></li>
-            <li class="list-item"><a href="/users/">Users</a></li>
+            <li class="list-item {{ (\Request::getRequestUri() == '/admin/') ? 'active' : '' }}"><a href="/admin/">Admin Panel Home</a></li>
+            <li class="list-item {{ (\Request::getRequestUri() == '/announcements/create/') ? 'active' : '' }}"><a href="/announcements/create/">Add Announcement</a></li>
+            <li class="list-item {{ (\Request::getRequestUri() == '/employees/create/') ? 'active' : '' }}"><a href="/employees/create/">Add Employee</a></li>
+            <li class="list-item {{ (\Request::getRequestUri() == '/announcements/') ? 'active' : '' }}"><a href="/announcements/">Announcements</a></li>
+            <li class="list-item {{ (\Request::getRequestUri() == '/employees/') ? 'active' : '' }}"><a href="/employees/">Employees</a></li>
+            <li class="list-item {{ (\Request::getRequestUri() == '/users/') ? 'active' : '' }}"><a href="/users/">Users</a></li>
         </ul>
     </div>
 </div>
+
+{{-- <li class="list-item {{ (request()->is('/admin/')) ? 'active' : '' }}"><a href="/admin/">Admin Panel Home</a></li> --}}
