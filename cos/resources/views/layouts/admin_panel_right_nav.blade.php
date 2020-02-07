0ae0b4598f099ad714fa2d89b9f259c7bcecc104
Kevin Decena <storm_iz_here@yahoo.com>
2020-02-07 20:07:01 +0800

    Updated admin panel links + styles.

--- cos/resources/views/layouts/admin_panel_right_nav.blade.php
+++ cos/resources/views/layouts/admin_panel_right_nav.blade.php
@@ -1,15 +1,14 @@
 <div class="card bg-primary admin-panel-card">
-    <div class="card-header admin-panel-card-header">Admin Panel</div>
-    <div class="card-body admin-panel-card-body-nav">
-        <ul class="list-unstyled">
-            <li class="list-item {{ (\Request::getRequestUri() == '/admin/') ? 'active' : '' }}"><a href="/admin/">Admin Panel Home</a></li>
-            <li class="list-item {{ (\Request::getRequestUri() == '/announcements/create/') ? 'active' : '' }}"><a href="/announcements/create/">Add Announcement</a></li>
-            <li class="list-item {{ (\Request::getRequestUri() == '/employees/create/') ? 'active' : '' }}"><a href="/employees/create/">Add Employee</a></li>
-            <li class="list-item {{ (\Request::getRequestUri() == '/announcements/') ? 'active' : '' }}"><a href="/announcements/">Announcements</a></li>
-            <li class="list-item {{ (\Request::getRequestUri() == '/employees/') ? 'active' : '' }}"><a href="/employees/">Employees</a></li>
-            <li class="list-item {{ (\Request::getRequestUri() == '/users/') ? 'active' : '' }}"><a href="/users/">Users</a></li>
-        </ul>
-    </div>
+	<div class="card-header admin-panel-card-header">Admin Panel</div>
+	<div class="card-body admin-panel-card-body-nav">
+		<ul class="list-unstyled">
+			<li class="list-item {{ (\Request::getRequestUri() == '/admin/') ? 'active' : '' }}"><a href="/admin/">Admin Panel Home</a></li>
+			<li class="list-item {{ (\Request::getRequestUri() == '/announcements/') ? 'active' : '' }}"><a href="/announcements/">Announcements</a></li>
+			<li class="list-item {{ (\Request::getRequestUri() == '/employees/') ? 'active' : '' }}"><a href="/employees/">Employees</a></li>
+			<li class="list-item {{ (\Request::getRequestUri() == '/users/') ? 'active' : '' }}"><a href="/users/">Users</a></li>
+			<hr>
+			<li class="list-item {{ (\Request::getRequestUri() == '/announcements/create/') ? 'active' : '' }}"><a href="/announcements/create/">Add Announcement</a></li>
+			<li class="list-item {{ (\Request::getRequestUri() == '/employees/create/') ? 'active' : '' }}"><a href="/employees/create/">Add Employee</a></li>
+		</ul>
+	</div>
 </div>
-
-{{-- <li class="list-item {{ (request()->is('/admin/')) ? 'active' : '' }}"><a href="/admin/">Admin Panel Home</a></li> --}}
