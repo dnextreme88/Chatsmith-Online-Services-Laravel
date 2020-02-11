115d08227be679d25c93d6540bde3a3301f996e8
Kevin Decena <storm_iz_here@yahoo.com>
2020-02-11 22:44:04 +0800

    Added User foreign key for TimeRecords table.

--- cos/database/migrations/2020_01_30_154049_create_time_records_table.php
+++ cos/database/migrations/2020_01_30_154049_create_time_records_table.php
@@ -16,13 +16,15 @@ class CreateTimeRecordsTable extends Migration
     {
         Schema::create('time_records', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('employee_id');
+            $table->foreign('user_id')->references('id')->on('user'); // Foreign key: User model
             $table->foreign('employee_id')->references('id')->on('employees'); // Foreign key: Employees model
             $table->string('time_of_shift');
             $table->date('date_of_shift');
             $table->string('employee_name');
             $table->dateTime('timestamp_in', 0);
-            $table->dateTime('timestamp_out', 0);
+            $table->dateTime('timestamp_out', 0)->nullable();
             $table->timestamps();
         });
     }
