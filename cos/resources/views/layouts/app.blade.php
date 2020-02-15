d46c6b3dc26565b93c09799a099afa8f5ac15318
Kevin Decena <storm_iz_here@yahoo.com>
2020-02-15 11:38:08 +0800

    Added leadforms links from header navbar.

--- cos/resources/views/layouts/app.blade.php
+++ cos/resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<!-- Scripts -->
-	<script src="{{ asset('js/app.js') }}" defer></script>
-	<script src="{{ asset('js/jquery.min.js') }}" defer></script>
+	<script src="{{ asset('js/app.js') }}"></script>
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
 
 	<!-- Fonts -->
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -38,6 +38,7 @@
 				<a class="navbar-brand" href="{{ url('/') }}">
 					Chatsmith Online Services
 				</a>
+
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 					<span class="navbar-toggler-icon"></span>
 				</button>
@@ -45,44 +46,56 @@
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<!-- Left Side Of Navbar -->
 					<ul class="navbar-nav mr-auto">
+						<li class="nav-item dropdown">
+								<a id="leadformsDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+									Leadforms
+								</a>
 
+								<div id="header-navbar-left" class="dropdown-menu dropdown-menu-left" aria-labelledby="leadformsDropdown">
+									<a class="dropdown-item" href="#">Focal Leadform</a>
+									<a class="dropdown-item" href="#">Plate IQ Leadform</a>
+									<a class="dropdown-item" href="#">PersistIQ Leadform</a>
+									<a class="dropdown-item" href="#">Smart Alto Leadform</a>
+								</div>
+
+						</li>
 					</ul>
 
 					<!-- Right Side Of Navbar -->
 					<ul class="navbar-nav ml-auto">
-						<!-- Authentication Links -->
-						@guest
+					<!-- Authentication Links -->
+					@guest
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+						</li>
+						@if (Route::has('register'))
 							<li class="nav-item">
-								<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+								<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
 							</li>
-							@if (Route::has('register'))
-								<li class="nav-item">
-									<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-								</li>
+						@endif
+					@else
+						<li class="nav-item dropdown">
+							<a id="userDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							@if (auth()->user()->image)
+								<img src="{{ asset(auth()->user()->image) }}" class="img-thumbnail rounded-circle avatar-thumbnail-extrasmall">
 							@endif
-						@else
-							<li class="nav-item dropdown">
-								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-									@if (auth()->user()->image)
-										<img src="{{ asset(auth()->user()->image) }}" class="img-thumbnail rounded-circle avatar-thumbnail-extrasmall">
-									@endif
-									{{ Auth::user()->name }} <span class="caret"></span>
+								{{ Auth::user()->name }} <span class="caret"></span>
+							</a>
+
+							<div id="header-navbar-right" class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+								<a class="dropdown-item" href="/profile">Profile</a>
+								<a class="dropdown-item" href="{{ route('logout') }}"
+								   onclick="event.preventDefault();
+												 document.getElementById('logout-form').submit();">
+									{{ __('Logout') }}
 								</a>
 
-								<div id="header-navbar-right" class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-									<a class="dropdown-item" href="/profile">Profile</a>
-									<a class="dropdown-item" href="{{ route('logout') }}"
-									   onclick="event.preventDefault();
-													 document.getElementById('logout-form').submit();">
-										{{ __('Logout') }}
-									</a>
-
-									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-										@csrf
-									</form>
-								</div>
-							</li>
-						@endguest
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+									@csrf
+								</form>
+							</div>
+						</li>
+					@endguest
 					</ul>
 				</div>
 			</div>
