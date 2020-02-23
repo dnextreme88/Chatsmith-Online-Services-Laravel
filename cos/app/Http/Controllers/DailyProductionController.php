a6b94f079dea231940f8a023d7d792f62dd9060e
Kevin Decena <storm_iz_here@yahoo.com>
2020-02-23 21:28:45 +0800

    Removed test variable.

--- cos/app/Http/Controllers/DailyProductionController.php
+++ cos/app/Http/Controllers/DailyProductionController.php
@@ -13,7 +13,6 @@ class DailyProductionController extends Controller
 	public function index() {
 		// Show daily productions for Chat Accounts, Focal and Plate
 		$date_today = Carbon::today(); // eg. 2020-02-14
-		$test_1 = ProductionChat::find(1)->created_at->format('Y-m-d');
 		$daily_productions_chat_accounts = ProductionChat::where('created_at', 'like', $date_today->format('Y-m-d') . '%')->orderBy('minutes_worked','desc')->get();
 		$daily_productions_focal = ProductionFocal::where('created_at', 'like', $date_today->format('Y-m-d') . '%')->orderBy('minutes_worked','desc')->get();
 		$daily_productions_plate = ProductionPlate::where('created_at', 'like', $date_today->format('Y-m-d') . '%')->orderBy('minutes_worked','desc')->get();
