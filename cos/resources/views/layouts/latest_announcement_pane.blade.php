ad5398df7ff3e1d519ef11f39a80d9879c95bf4c
Kevin Decena <storm_iz_here@yahoo.com>
2020-03-04 00:35:34 +0800

    Added view all announcements link on latest announcement pane.

--- cos/resources/views/layouts/latest_announcement_pane.blade.php
+++ cos/resources/views/layouts/latest_announcement_pane.blade.php
@@ -2,3 +2,4 @@
 <p class="text-left">Posted by <a href="announcements/user/{{ $latest_announcement->user->username }}">{{ $latest_announcement->user->username }}</a> on <strong>{{ \Carbon\Carbon::parse($latest_announcement->created_at)->format('F j, Y g:i:s A') }}</strong></p>
 <h3>{{ $latest_announcement->title }}</h3>
 <p id="announcement-pane-description">{{ $latest_announcement->description }}</p>
+<p class="text-right"><a href="{{ route('announcements.index') }}">View all announcements</a></p>
