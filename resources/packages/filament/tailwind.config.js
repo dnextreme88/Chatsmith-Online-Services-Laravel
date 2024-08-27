e4b26f7917af4b5fbe0828d38df2fd5c5dc77b69
Kevin Decena <storm_iz_here@yahoo.com>
2024-08-27 22:45:28 +0800

    add Tailwind CSS support in Filament admin panels

---
+++ resources/packages/filament/tailwind.config.js
@@ -0,0 +1,8 @@
+import preset from '../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './vendor/filament/**/*.blade.php'
+    ],
+}
