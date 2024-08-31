485997d9ce9e81c0b8a53f92b631b723a212c256
Kevin Decena <storm_iz_here@yahoo.com>
2024-08-31 22:40:41 +0800

    add icon to create action

--- app/Filament/Resources/AnnouncementResource/Pages/ListAnnouncement.php
+++ app/Filament/Resources/AnnouncementResource/Pages/ListAnnouncement.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\AnnouncementResource\Pages;
 
 use App\Filament\Resources\AnnouncementResource;
-use Filament\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAnnouncement extends ListRecords
@@ -13,7 +13,8 @@ class ListAnnouncement extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make()
+                ->icon('heroicon-o-plus'),
         ];
     }
 }
