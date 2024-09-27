4049462ac4f1496046f63c62fe653e91cf2a3b47
Kevin Decena <storm_iz_here@yahoo.com>
2024-09-28 01:40:38 +0800

    fix layout inconsistencies between original layout and app layout to also support extending layouts for components being used as routes in web.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+
+        <meta name="application-name" content="{{ config('app.name') }}" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -11,11 +13,20 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+
         <!-- Styles -->
         @filamentStyles
         @livewireStyles
         @vite('resources/css/app.css')
+
+        @stack('styles')
     </head>
+
     <body class="font-sans antialiased">
         <x-banner />
 
@@ -24,17 +35,13 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
+                <header class="bg-white shadow dark:bg-gray-800">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">{{ $header }}</div>
                 </header>
             @endif
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <main>{{ $slot }}</main>
         </div>
 
         @stack('modals')
@@ -43,5 +50,7 @@
         @filamentScripts
         @livewireScripts
         @vite('resources/js/app.js')
+
+        @stack('scripts')
     </body>
 </html>
--- resources/views/livewire/app-layout.blade.php
+++ resources/views/livewire/app-layout.blade.php
@@ -7,7 +7,11 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>{{ config('app.name') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        {{-- Fonts --}}
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <style>
             [x-cloak] {
@@ -19,14 +23,36 @@
         @filamentStyles
         @livewireStyles
         @vite('resources/css/app.css')
+
+        @stack('styles')
     </head>
 
     <body x-data="themeSwitcher()" x-bind:class="{'dark': switchOn}" class="antialiased">
-        {{ $slot }}
+        <x-banner />
+
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @livewire('navigation-menu')
+
+            {{-- Page Heading --}}
+            @if (isset($header))
+                <header class="bg-white shadow dark:bg-gray-800">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">{{ $header }}</div>
+                </header>
+            @endif
+
+            {{-- Page Content --}}
+            <main>
+                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">{{ $slot }}</div>
+            </main>
+        </div>
+
+        @stack('modals')
 
         {{-- Scripts --}}
         @filamentScripts
-        @livewireStyles
+        @livewireScripts
         @vite('resources/js/app.js')
+
+        @stack('scripts')
     </body>
 </html>
