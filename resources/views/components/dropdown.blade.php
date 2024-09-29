45f5df424e8fd3e5f1b7bf3b14d8a4e951830227
Kevin Decena <storm_iz_here@yahoo.com>
2024-09-30 02:28:36 +0800

    add props key + refactor Tailwind classes by re-ordering them

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,37 +1,42 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700', 'dropdownClasses' => ''])
+@props([
+    'align' => 'right',
+    'width' => '48',
+    'content_classes' => 'py-1 bg-white dark:bg-gray-700',
+    'dropdown_classes' => '',
+    'toggle_dropdown_when_clicking_inside' => true
+])
 
 @php
-$alignmentClasses = match ($align) {
-    'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
-    'top' => 'origin-top',
-    'none', 'false' => '',
-    default => 'ltr:origin-top-right rtl:origin-top-left end-0',
-};
+    $alignment_classes = match($align) {
+        'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
+        'top' => 'origin-top',
+        'none', 'false' => '',
+        default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+    };
 
-$width = match ($width) {
-    '48' => 'w-48',
-    '60' => 'w-60',
-    default => 'w-48',
-};
+    $width = match($width) {
+        '48' => 'w-48',
+        '60' => 'w-60',
+        default => 'w-48',
+    };
 @endphp
 
-<div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
+<div class="relative" x-data="{ open: false }" x-on:click.away="open = false" x-on:close.stop="open = false">
+    <div x-on:click="open = !open">
         {{ $trigger }}
     </div>
 
     <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} {{ $dropdownClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{ $content }}
-        </div>
+        x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="transform opacity-0 scale-95"
+        x-transition:enter-end="transform opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-75"
+        x-transition:leave-start="transform opacity-100 scale-100"
+        x-transition:leave-end="transform opacity-0 scale-95"
+        class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignment_classes }} {{ $dropdown_classes }}"
+        style="display: none;"
+        {!! $toggle_dropdown_when_clicking_inside ? 'x-on:click="open = false"' : '' !!}
+    >
+        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $content_classes }}">{{ $content }}</div>
     </div>
 </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,12 +1,12 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 dark:bg-gray-800 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex items-center shrink-0">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+                        <x-application-mark class="block w-auto h-9" />
                     </a>
                 </div>
 
@@ -20,19 +20,19 @@
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Settings Dropdown -->
-                <div class="ms-3 relative">
-                    <x-dropdown align="right" width="48">
+                <div class="relative ms-3">
+                    <x-dropdown :align="'right'" :width="48" :toggle_dropdown_when_clicking_inside="false">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->last_name }}" />
+                                <button class="flex text-sm transition border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300">
+                                    <img class="object-cover h-8 w-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->last_name }}" />
                                 </button>
                             @else
                                 <span x-bind:class="{'dark:bg-gray-700': open}" class="inline-flex items-center rounded-md">
-                                    <button class="inline-flex items-center p-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 dark:hover:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150" type="button">
-                                        <span class="dark:text-gray-400 pl-1 pr-3">{{ Auth::user()->last_name }}</span>
+                                    <button class="inline-flex items-center p-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700" type="button">
+                                        <span class="pl-1 pr-3 dark:text-gray-400">{{ Auth::user()->last_name }}</span>
 
-                                        <svg x-bind:class="{'rotate-180': open}" class="h-4 w-4 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <svg x-bind:class="{'rotate-180': open}" class="h-4 w-4 transition duration-150 ease-in-out" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                         </svg>
                                     </button>
@@ -44,7 +44,7 @@
                             {{-- Dark mode toggle --}}
                             <div class="block px-4 pt-2 text-xs text-gray-400">Dark Mode</div>
 
-                            <div x-data="window.darkModeSwitcher()" x-init="init" @keydown.window.tab="switchOn = false" class="flex place-content-start px-2 py-2 space-x-2">
+                            <div x-data="window.darkModeSwitcher()" x-init="init" @keydown.window.tab="switchOn = false" class="flex px-2 py-2 space-x-2 place-content-start">
                                 <input x-bind:checked="switchOn" class="hidden" type="checkbox" name="switch" />
 
                                 <button
@@ -54,7 +54,7 @@
                                     class="relative inline-flex h-5 py-0.5 ml-2 focus:outline-none rounded-full w-9"
                                     type="button"
                                 >
-                                    <span x-bind:class="switchOn ? 'translate-x-[18px]' : 'translate-x-0.5'" class="w-4 h-4 duration-200 ease-in-out bg-white rounded-full shadow-md"></span>
+                                    <span x-bind:class="switchOn ? 'translate-x-[18px]' : 'translate-x-0.5'" class="h-4 w-4 duration-200 ease-in-out bg-white rounded-full shadow-md"></span>
                                 </button>
 
                                 <label
@@ -100,8 +100,8 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
-                <button x-on:click="open = !open" x-bind:class="{'focus:rotate-90': open}" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+            <div class="flex items-center -me-2 sm:hidden">
+                <button x-on:click="open = !open" x-bind:class="{'focus:rotate-90': open}" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path x-bind:class="{'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path x-bind:class="{'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -124,22 +124,22 @@
             <div class="flex items-center px-4">
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="shrink-0 me-3">
-                        <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->last_name }}" />
+                        <img class="object-cover h-10 w-10 rounded-full" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->last_name }}" />
                     </div>
                 @endif
 
                 <div>
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->last_name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="text-base font-medium text-gray-800 dark:text-gray-200">{{ Auth::user()->last_name }}</div>
+                    <div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
             </div>
 
             <div class="mt-3 space-y-1">
                 {{-- Dark mode toggle --}}
                 <div class="pt-4 pb-1 border-t border-blue-200 dark:border-slate-600">
-                    <div class="ps-4 space-y-1 text-slate-400 text-sm">Dark Mode</div>
+                    <div class="space-y-1 text-sm ps-4 text-slate-400">Dark Mode</div>
 
-                    <div x-data="window.darkModeSwitcher()" x-init="init" x-on:keydown.window.tab="switchOn = false" class="flex place-content-start px-2 py-2 space-x-2">
+                    <div x-data="window.darkModeSwitcher()" x-init="init" x-on:keydown.window.tab="switchOn = false" class="flex px-2 py-2 space-x-2 place-content-start">
                         <input x-bind:checked="switchOn" class="hidden" type="checkbox" name="switch" />
 
                         <button
@@ -149,7 +149,7 @@
                             class="relative inline-flex h-6 py-0.5 ml-2 focus:outline-none rounded-full w-10"
                             type="button"
                         >
-                            <span x-bind:class="switchOn ? 'translate-x-[18px]' : 'translate-x-0.5'" class="w-5 h-5 duration-200 ease-in-out bg-white rounded-full shadow-md"></span>
+                            <span x-bind:class="switchOn ? 'translate-x-[18px]' : 'translate-x-0.5'" class="h-5 w-5 duration-200 ease-in-out bg-white rounded-full shadow-md"></span>
                         </button>
 
                         <label
