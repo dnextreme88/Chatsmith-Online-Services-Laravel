79335e32f19c0f9e5d2b82af4df29a3ca2fc871a
Kevin Decena <storm_iz_here@yahoo.com>
2024-10-07 22:32:06 +0800

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,11 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="{{ asset('images/cos-favicon.ico') }}" />
+
+        {{-- Fonts --}}
+        <link rel="preconnect" href="https://fonts.bunny.net" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <style>
