725e5f68f2e677ec5889d3a896082d81bf076021
Kevin Decena <storm_iz_here@yahoo.com>
2024-10-19 21:51:38 +0800

    fix value of date_resigned field if is_active is 0

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -15,6 +15,9 @@ class EmployeeFactory extends Factory
 {
     public function definition(): array
     {
+        $is_active = fake()->randomElement([0, 1]);
+        $date_resigned = $is_active == 0 ? fake()->dateTimeBetween('-4 years') : null;
+
         return [
             'user_id' => User::factory(),
             'employee_number' => fake()->numberBetween(1, 99999),
@@ -22,8 +25,8 @@ class EmployeeFactory extends Factory
             'designation' => fake()->randomElement(OfficeDesignations::cases())->value,
             'role' => fake()->randomElement(EmployeeRoles::cases())->value,
             'date_hired' => fake()->dateTimeBetween('-8 years', '-6 years'),
-            'date_resigned' => fake()->dateTimeBetween('-4 years'),
-            'is_active' => fake()->randomElement([0, 1]),
+            'date_resigned' => $date_resigned,
+            'is_active' => $is_active,
             // Values are based on actual format
             'pag_ibig_number' => fake()->numerify('############'),
             'philhealth_number' => fake()->numerify('####-####-####'),
