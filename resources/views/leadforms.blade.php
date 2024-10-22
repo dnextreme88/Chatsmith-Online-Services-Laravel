5497fd740ccd6ecb7c3987d57e96d8cdafebd102
Kevin Decena <storm_iz_here@yahoo.com>
2024-10-23 01:06:06 +0800

    refactor filenames of logos

--- resources/views/leadforms.blade.php
+++ resources/views/leadforms.blade.php
@@ -25,17 +25,17 @@
             class="grid grid-cols-1 space-y-6 max-w-7xl lg:space-y-0 lg:grid-cols-3 lg:place-items-center lg:h-75-vh"
         >
             <div class="grid gap-3 p-3 transition-all duration-300 bg-gray-300 border-gray-300 place-items-center rounded-xl dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-400 dark:hover:bg-gray-700">
-                <img src="{{ asset('images/leadform_smart_alto_logo.webp') }}" class="h-52 w-52" alt="Smart Alto logo" title="Smart Alto logo" />
+                <img src="{{ asset('images/logo_smart_alto.webp') }}" class="h-52 w-52" alt="Smart Alto logo" title="Smart Alto logo" />
                 <p x-on:click="showLeadform('chat')" class="text-orange-800 dark:text-orange-200 hover:underline hover:cursor-pointer">Proceed to Chat Account Leadform</p>
             </div>
 
             <div class="grid gap-3 p-3 transition-all duration-300 bg-gray-300 border-gray-300 place-items-center rounded-xl dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-400 dark:hover:bg-gray-700">
-                <img src="{{ asset('images/leadform_focal_systems_logo.webp') }}" class="h-52 w-52" alt="Focal Systems logo" title="Focal Systems logo" />
+                <img src="{{ asset('images/logo_focal_systems.webp') }}" class="h-52 w-52" alt="Focal Systems logo" title="Focal Systems logo" />
                 <p x-on:click="showLeadform('focal')" class="text-orange-800 dark:text-orange-200 hover:underline hover:cursor-pointer">Proceed to Focal Leadform</p>
             </div>
 
             <div class="grid gap-3 p-3 transition-all duration-300 bg-gray-300 border-gray-300 place-items-center rounded-xl dark:border-gray-700 dark:bg-gray-800 hover:bg-gray-400 dark:hover:bg-gray-700">
-                <img src="{{ asset('images/leadform_plate_iq_logo.webp') }}" class="h-52 w-52" alt="Plate IQ logo" title="Plate IQ logo" />
+                <img src="{{ asset('images/logo_plate_iq.webp') }}" class="h-52 w-52" alt="Plate IQ logo" title="Plate IQ logo" />
                 <p x-on:click="showLeadform('plate')" class="text-orange-800 dark:text-orange-200 hover:underline hover:cursor-pointer">Proceed to Plate Leadform</p>
             </div>
         </div>
